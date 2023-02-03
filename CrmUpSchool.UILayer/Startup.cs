228c47b53bedc278c67552dd9114a915aa213712
cansu bican <cansubican11@gmail.com>
2023-02-03 17:22:59 +0300

    UpSchool_Myblog

--- CrmUpSchool.UILayer/Startup.cs
+++ CrmUpSchool.UILayer/Startup.cs
@@ -60,7 +60,7 @@ namespace CrmUpSchool.UILayer
             {
                 options.LogoutPath = "/Login/Index";
             });
-            }
+        }
             
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
